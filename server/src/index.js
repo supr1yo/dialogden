02f51c0e7c395b203380f5abcd8c4f3d50240af5
supr1yo <supriyo.developer@gmail.com>
2024-06-26 17:26:40 +0530

    Initial commit

---
+++ server/src/index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import { createServer } from 'node:http';
+import dotenv from 'dotenv';
+import { Server } from 'socket.io';
+import cors from 'cors';
+const PORT = process.env.SERVER_PORT || 8080;
+dotenv.config();
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:3000'
+    }
+});
+
+
+app.use(cors());
+app.get('/', (req, res) => {
+    res.status(201).json({
+        message: 'Welcome to the backend of Dialog Den! Didn\'t expect you to be here 🤔'
+    });
+});
+
+io.on('connection', (socket) => {
+    console.log(`👋 Client connected: ${socket.id}`);
+
+    socket.on('disconnect', (reason) => {
+        console.log(`⚠️ Disconnected due to - ${reason}`)
+    })
+})
+
+server.listen(PORT, () => {
+    console.log(`Running on http://localhost:${PORT}`);
+});
+
