bb2c287790c0bb481cac43a74ad105c998efb4ab
supr1yo <supriyo.developer@gmail.com>
2024-06-27 18:02:51 +0530

    renamed

--- client/src/pages/chat.tsx
+++ client/src/pages/chat.tsx
@@ -1,17 +1,27 @@
-import  io  from 'socket.io-client';
+
+import { useState, useEffect } from 'react';
+import io from 'socket.io-client';
 
 const socket = io('http://localhost:8080');
 
 export default function Chat() {
 
+    const [message, setMessage] = useState('');
     const sendMessage = () => {
         // Sending message to the server
         socket.emit('send_message', {message: 'hi'})
     }
+
+    useEffect(() => {
+        socket.on('receive_message', (data) => {
+            setMessage(data);
+        })
+    })
     return (
         <div>
             <input type="text" name="" id="" />
             <button onClick={sendMessage}>Send</button>
+            {message}
         </div>
     )
 }
